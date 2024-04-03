d8677525a2d777947b89a928c9c3347c63255a06
Vijay Chopra <vijaychopra627@gmail.com>
2024-04-03 17:06:53 +0530

    vijay

---
+++ script.js
@@ -0,0 +1,27 @@
+//for text to voice button
+let speech= new SpeechSynthesisUtterance();
+let textarea=document.querySelector("textarea")
+let button = document.querySelector("button");
+
+
+button.addEventListener("click",()=>{
+    speech.text= textarea.value;
+    window.speechSynthesis.speak(speech);
+})
+
+
+//for change voice language
+let voices= [];
+let voiceselect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = ()=>{
+    voices = window.speechSynthesis.getVoices();    
+    speech.voice= voices[-1];
+   
+    //to add options for select tag
+    voices.forEach((voice,i) =>(voiceselect.options[i] = new Option(voice.name, i)));
+}
+
+voiceselect.addEventListener("change",()=>{
+    speech.voice =voices[voiceselect.value];
+})
